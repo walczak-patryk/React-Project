0c51af2822bf0c3944bac2bf31e06389cae2c168
wizkiz <szy.majorek@gmail.com>
2020-01-14 21:24:56 +0100

    bookings list update

--- front-end-project/src/App.js
+++ front-end-project/src/App.js
@@ -6,7 +6,7 @@ import {
   Link,
 } from 'react-router-dom'
 
-import Test from './test'
+import Bookings from './BookingsPage'
 import Details from './Details'
 import logo from './logo.svg';
 import './App.css';
@@ -15,11 +15,13 @@ import LoginPage from './LoginPage'
 function App() {
   return (
     <div className="App">
+      <div className="Title">
+        <h2>Bookly - Reservation History</h2>
+      </div>
       <Router>
         <Switch>
-          <Route exact path="/" component={LoginPage}/> 
-          <Route exact path="/bookings" component={Test}/>
-          <Route exact path="/bookings/:id" component={Details}/> 
+          <Route exact path="/" component={LoginPage} />
+          <Route exact path="/bookings" component={Bookings} />
         </Switch>
       </Router>
     </div>
--- front-end-project/src/Booking.js
+++ front-end-project/src/Booking.js
@@ -1,41 +1,79 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { withRouter } from "react-router-dom";
+import Details from './Details'
 import './Booking.css';
 
 class Booking extends React.Component {
-    constructor(props) {
-        super(props);
-        props.booking.start_date = new Date(props.booking.start_date);
+  constructor(props) {
+    super(props);
+    props.booking.start_date = new Date(props.booking.start_date);
+    this.state = {
+      showDetails: false,
+      detailsLoaded: false,
+      detailsLoading: false,
+      details: null
     }
-    element = (value) => {
-        return (
-            <div className="Element">{value}</div>
-        )
+    this.buttonHandler = this.buttonHandler.bind(this)
+    this.loadDetails = this.loadDetails.bind(this)
+  }
+
+  element = (value) => {
+    return (
+      <div className="Element">{value}</div>
+    )
+  }
+
+  buttonHandler = () => {
+    this.setState(prevState => ({
+      showDetails: !prevState.showDetails
+    }))
+    this.loadDetails();
+  }
+
+  loadDetails() {
+    if (this.state.detailsLoaded) {
+      return;
     }
+    this.setState({
+      detailsLoading: true
+    });
+    //console.log(this.props.booking.type)
+    fetch(`http://localhost:3004/${this.props.booking.type}/${this.props.booking.item_id}`)
+      .then(response => response.json())
+      .then(data => this.setState({ details: data }))
+      .then(() => this.setState({ detailsLoading: false, detailsLoaded: true }));
+  }
 
-    render() {
-        const {booking} = this.props
+  render() {
+    const { booking } = this.props
 
-        const Button = (
-            <Link to={`bookings/${booking.booking_id}`}>
-                <button>XD</button>
-            </Link>
-        )
+    const myButton = (
+      <button onClick={this.buttonHandler}>XD</button>
+    )
 
-        return (
-            <div className={this.props.className}>
-                {this.element(booking.booking_id)}
-                {this.element(booking.user_id)}
-                {this.element(booking.item_id)}
-                {this.element(booking.type)}
-                {this.element(booking.active.toString())}
-                {this.element(`${booking.start_date.getDate()} 
-                    ${booking.start_date.getMonth()+1} ${booking.start_date.getFullYear()}`)}
-                {this.element(Button)}
-            </div>
-        )
-    }
+    const preDetails = (
+      this.state.detailsLoading ? <div style={{ marginTop: "0.5%" }}>Loading details...</div> : <Details data={this.state.details} itemType={booking.type} />
+    )
+
+    return (
+      <div>
+        <div className={this.props.className}>
+          {this.element(booking.booking_id)}
+          {this.element(booking.user_id)}
+          {this.element(booking.username)}
+          {this.element(booking.item_id)}
+          {this.element(booking.type)}
+          {this.element(booking.item_info)}
+          {this.element(booking.active.toString())}
+          {this.element(`${booking.start_date.getDate()} 
+                    ${booking.start_date.getMonth() + 1} ${booking.start_date.getFullYear()}`)}
+          {this.element(myButton)}
+        </div>
+        {this.state.showDetails && preDetails}
+      </div>
+    )
+  }
 }
 
 export default withRouter(Booking)
