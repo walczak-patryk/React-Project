c30aed5cd1f34c2cccd4a48d85a3e4928de9a3c5
Zalinafali <zalinafali@gmail.com>
2020-01-30 15:49:28 +0100

    Token working 1.1  - catching JWT token exceptions  - catching username exceptions  - using token in mobile app (not working, without tests)

--- central-app-backend/src/main/java/central/app/backend/centralapp/CentralAppApplication.java
+++ central-app-backend/src/main/java/central/app/backend/centralapp/CentralAppApplication.java
@@ -13,13 +13,4 @@ public class CentralAppApplication {
 		SpringApplication.run(CentralAppApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
 }
